380cf0ff87bd52f09220924852f4bd7670a41b3b
asifalam515 <asibulalam515@gmail.com>
2024-12-05 16:30:02 +0600

    catch all route

--- src/app/products/[productId]/page.js
+++ src/app/products/[productId]/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const DynamicPage = ({ params }) => {
+const DynamicPage = ({ params, searchParams }) => {
+  console.log(searchParams);
   return (
     <div>
       <h1 className="text-4xl">Dynamic Product Page {params.productId} </h1>
