9a91347f3a0a36e123f3a07133db6fce6285f7c4
asifalam515 <asibulalam515@gmail.com>
2024-12-06 11:23:06 +0600

    server to client component converted

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
-import Image from "next/image";
+"use client";
 import Link from "next/link";
 import Header from "./components/header";
+import { useState } from "react";
 
 export default function Home() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="text-center">
       <Header></Header>
       <h1 className="text-center text-3xl p-2 m-2">
-        Lets get Started with next js
+        Lets get Started with next js.Counter {counter}
       </h1>
       <p>
         Know About us <Link href="/about">Here</Link>{" "}
@@ -15,6 +18,18 @@ export default function Home() {
       <p>
         Here is the <Link href="footer">Footer</Link>{" "}
       </p>
+      <button
+        className="btn btn-primary"
+        onClick={() => setCounter(counter + 1)}
+      >
+        Increase
+      </button>
+      <button
+        className="btn btn-warning"
+        onClick={() => setCounter(counter - 1)}
+      >
+        Decrease
+      </button>
     </div>
   );
 }
